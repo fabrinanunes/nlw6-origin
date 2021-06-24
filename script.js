0fb57e474e37b477e4b2b6a81fcbee55109da28a
fabrinanunes <84260347+fabrinanunes@users.noreply.github.com>
2021-06-23 22:14:32 -0300

    3º dia

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-/*abre e fecha o menu hamburguer*/
+/* abre e fecha o menu hamburguer */
 const nav = document.querySelector('#header nav');
 const toggle = document.querySelectorAll("nav .toggle");
 
-for(const element of toggle){
-    element.addEventListener('click', function(){
+for (const element of toggle) {
+    element.addEventListener('click', function () {
         nav.classList.toggle('show')
     })
 };
 
-/* clicar no item do menu*/
+/* clicar no item do menu */
 const links = document.querySelectorAll('nav ul li a');
 
-for (const link of links){
-    link.addEventListener('click', function(){
+for (const link of links) {
+    link.addEventListener('click', function () {
         nav.classList.remove('show');
     })
 }
 
-/*Mudar o header com scroll*/
+/* mudar o header com scroll */
 
 const header = document.querySelector("#header");
 const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', function(){
-    if(this.window.scrollY >= navHeight){
+window.addEventListener('scroll', function () {
+    if (this.window.scrollY >= navHeight) {
         //scroll maior que altura do header
         header.classList.add('scroll')
     } else {
@@ -31,3 +31,31 @@ window.addEventListener('scroll', function(){
         header.classList.remove('scroll')
     }
 })
+
+/* caixa de depoimentos com swiper */
+
+const swiper = new Swiper('.swiper-container', {
+    sliderPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+})
+
+/* imagem durante rolagem */
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .text, #home .image,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links`,
+    { interval: 100 })
